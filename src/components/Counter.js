d65b13b89929a64c8e9649c03ac4ee0b360b61bf
Nicola Barbaro <barbaronico93@gmail.com>
2025-01-09 17:51:46 +0100

    Added some trial actions to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,1 +1,38 @@
+import classes from './Counter.module.css';
+import { useSelector, useDispatch, connect } from 'react-redux';
 
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  }
+  const increaseByN = () => {
+    dispatch({ type: 'increase', value: 5 });
+  }
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  }
+  const reset = () => {
+    dispatch({ type: 'reset' });
+  }
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseByN}>Increment By N</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
