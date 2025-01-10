a69b1c7b1e5d4457eafca784a94c1cfbb205c33d
Nicola Barbaro <barbaronico93@gmail.com>
2025-01-10 13:00:04 +0100

    Introducted createSlice & Redux Toolkit and split the code by slices

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,21 @@
 import classes from './Auth.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/authSlice';
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(authActions.login());
+  }
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
@@ -13,7 +24,9 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          {!isAuthenticated && (
+            <button>Login</button>)
+          }
         </form>
       </section>
     </main>
